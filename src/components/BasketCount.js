a650e6839693fec87556773ce4355d896fe6a555
Damilola <opemipoolagunju@gmail.com>
2023-01-15 22:30:51 +0000

    fix: basket.js fix

--- src/components/BasketCount.js
+++ src/components/BasketCount.js
@@ -3,7 +3,7 @@ import React from "react";
 function BasketCount(props) {
   return (
     <div className="basket-count">
-      {props.basketCount} item{props.basketCount !== 0 ? "k" : ""}
+      {props.basketCount} item{props.basketCount !== 1 ? "k" : ""}
     </div>
   );
 }
