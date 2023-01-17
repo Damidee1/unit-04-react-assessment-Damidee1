718d77451fd3edbb0ff2b1110c619c60ec5aad35
Genyus <genyus@gmail.com>
2023-01-17 04:09:21 +0000

    test: updates tests and README.md

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,9 +1,8 @@
-import React from "react";
-import createRoot from "react-dom/client";
+import React from 'react';
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../../src/App";
 
 it("renders <App/> without crashing", () => {
-  const div = document.createElement("div");
-  createRoot(<App />, div);
+  render(<App />);
 });
